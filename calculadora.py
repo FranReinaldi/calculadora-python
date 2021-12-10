6a93bca80b50dbe338346239bc1917e9bb57a783
Tincho <franco.reinaldi@hotmail.com>
2021-12-09 21:25:11 -0300

    arreglando mi cagada

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def calculadora(nro1, nro2, operador):
+	if operador == "+":
+		print(nro1+nro2)
+	elif operador == "-":
+		print(nro1-nro2)
+	elif operador == "*":
+		print(nro1*nro2)
+	elif operador == "/":
+		print(nro1/nro2)
+	else:
+		print("El operador no es valido")
